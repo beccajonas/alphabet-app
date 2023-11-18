2234a26ee9cc7643b72b3ceaf74d5473072647f3
Becca Jonas <beccajonas3@gmail.com>
2023-11-18 17:10:45 -0500

    add server code

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
 // * To do: 
 // * 1. Persist patch without refresh
 // * 2. Remove selected element from relatedWords array on delete button click
